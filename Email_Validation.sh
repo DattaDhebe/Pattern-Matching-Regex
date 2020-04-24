c59e4ba0d2c924cefc1faef2b586fb7d2324e5cc
DattaDhebe <dattadhebe75@gmail.com>
2020-04-24 19:35:51 +0530

    [Datta]Refactor Added 'useCase5' fith optional part with restriction of only two character

--- Email_Validation.sh
+++ Email_Validation.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 echo "enter email address : "
 read email
 
-emailPattern="^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}$" 
+emailPattern="^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$" 
 
 if [[ $email =~ $emailPattern ]]
 then
